1f455910355287b96cd440d037d8c87109a95fdf
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-08-23 23:11:32 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,32 +1,72 @@
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  1 + rand(11)
 end
 
-def display_card_total
+def display_card_total(num)
+  
+  puts "Your cards add up to #{num}"
+  
+ 
+  
+  
   # code #display_card_total here
 end
 
 def prompt_user
   # code #prompt_user here
+  
+  puts "Type 'h' to hit or 's' to stay"
+  
+  
+  
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(num)
+  
+  puts "Sorry, you hit #{num}. Thanks for playing!"
   # code #end_game here
 end
 
 def initial_round
+  first_round = deal_card + deal_card
+  
+  display_card_total(deal_card)
+  
+  first_round
+ 
+  
+  
+  
   # code #initial_round here
-end
+ end 
 
-def hit?
+
+def hit?(num)
+  
+  prompt_user
+  input = get_user_input
+  if input == "h"
+    num + deal_card
+  elsif input == "s"
+   num
+  end
+ 
+    
+  
+  
+  
+ 
   # code hit? here
 end
 
@@ -41,4 +81,4 @@ end
 def runner
   # code runner here
 end
-    
+
